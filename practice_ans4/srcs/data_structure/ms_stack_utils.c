697eae397e91d123c402da0b72213500e5e00b18
ntan-wan <wangtanngan@gmail.com>
2023-02-21 01:59:42 +0800

    update

---
+++ practice_ans4/srcs/data_structure/ms_stack_utils.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/25 09:25:40 by ntan-wan          #+#    #+#             */
+/*   Updated: 2022/12/25 14:42:33 by ntan-wan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stack.h"
+
+t_stack	*stack_create(int max)
+{
+	int		i;
+	t_stack	*new_stack;
+
+	new_stack = malloc(sizeof(t_stack));
+	new_stack->top = -1;
+	new_stack->max_size = max;
+	new_stack->arr = ft_calloc(max + 1, sizeof(char));
+	return (new_stack);
+}
+
+bool	stack_is_full(t_stack *stack)
+{
+	return (stack->top == stack->max_size - 1);
+}
+
+bool	stack_is_empty(t_stack *stack)
+{
+	return (stack->top == -1);
+}
+
+void	stack_push(t_stack *stack, char item)
+{
+	if (stack_is_full(stack))
+		return ;
+	stack->arr[++stack->top] = item;
+}
+
+void	stack_pop(t_stack *stack)
+{
+	if (stack_is_empty(stack))
+		return ;
+	stack->arr[stack->top] = '\0';
+	stack->top--;
+}
