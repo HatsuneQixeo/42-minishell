a29fda57bfec3339a1b4db05b23256e19e83b571
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-10-27 21:59:04 +0800

    libft update

--- srcs/utils/log.c
+++ srcs/utils/log.c
@@ -18,7 +18,7 @@ int	ms_errlog(const char *str_format, ...)
 	int		ret;
 
 	va_start(args, str_format);
-	ret = ft_putstr_fd(MINISHELL": ", 2) + ft_printf_core(2, str_format, args);
+	ret = ft_putstr_fd(MINISHELL": ", 2) + ft_printf_core(2, str_format, &args);
 	va_end(args);
 	return (ret);
 }
